0bb2894017f87f6eb17ba9438a899f2e84c7f614
ndittren <36773036+ndittren@users.noreply.github.com>
2023-05-18 15:13:25 -0400

    add proper links and style nav

--- src/nav.tsx
+++ src/nav.tsx
@@ -1,51 +1,59 @@
 import React from 'react';
 import Logo from './images/logo.svg';
+import {Link} from 'react-router-dom';
 
 export const Nav: React.FC = () => {
 
     return (
         <>
-            <nav id="nav-global" className="navbar navbar-expand-lg bg-dark"
-                data-bs-theme="dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={Logo} alt="Logo" height="80"
-                            className="d-inline-block align-text-top"></img>
-                    </a>
-                    <button className="navbar-toggler" type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+            <nav id='nav-global'
+                className='navbar navbar-expand-lg navbar-dark bg-dark'
+                data-bs-theme='dark'>
+                <div className='container-fluid'>
+                    <Link to='/' className='navbar-brand'>
+                        <img src={Logo} alt='Logo' height='80'
+                            className='d-inline-block align-text-top'></img>
+                    </Link>
+                    <button className='navbar-toggler' type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarSupportedContent'
+                        aria-controls='navbarSupportedContent'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
                     </button>
-                    <div className="collapse navbar-collapse"
-                        id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Introduction
-                                </a>
+                    <div className='collapse navbar-collapse'
+                        id='navbarSupportedContent'>
+                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                            <li className='nav-item'>
+                                <Link to='/'
+                                    className='nav-link'>
+                                    Introduction
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link"
-                                    href="/descriptive">Descriptive Statistics
-                                </a>
+                            <li className='nav-item'>
+                                <Link to='/descriptive'
+                                    className='nav-link'>
+                                    Descriptive Statistics
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link"
-                                    href="/inferential">Inferential Statistics
-                                </a>
+                            <li className='nav-item'>
+                                <Link to='/inferential'
+                                    className='nav-link'>
+                                    Inferential Statistics
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link"
-                                    href="/comparative">Comparing Genres
-                                </a>
+                            <li className='nav-item'>
+                                <Link to='/comparative'
+                                    className='nav-link'>
+                                    Comparing Genres
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link"
-                                    href="/confidence">Confidence Intervals
-                                </a>
+                            <li className='nav-item'>
+                                <Link to='/confidence'
+                                    className='nav-link'>
+                                    Confidence Intervals
+                                </Link>
                             </li>
                         </ul>
                     </div>
