bf279d453a5691a1120c9dd60336b034e1d52940
Evan Petersen <emp2221@columbia.edu>
2023-09-05 15:29:30 -0400

    Fixed Descriptive Statistics formatting

--- src/descriptiveStats.tsx
+++ src/descriptiveStats.tsx
@@ -12,28 +12,29 @@ const descriptiveTabs: TabData[] = [
 ];
 
 const questions: string[] = [
-    'What does the x-axis of the histogram represent? What does the y-axis\
-        represent?',
-    'Select any two Beats Per Minute (BPM) values on the x-axis. How many data\
-        points have BPM values within that range?',
-    'The mean is the average value of a distribution of data points, and the\
-        standard deviation is a measure of how dispersed the data is in\
-        relation to the mean. Estimate the mean and standard deviation of your\
-        sample.',
-    'A new histogram is produced after every 100 data points. Draw additional\
-        data points to get you to 100 and take a screenshot of your histogram.\
-        Then draw another 100 data points, and paste a screenshot of the new\
-        histogram below. Is it identical to the first one? Why or why not?',
-    'Comparing the two histograms from question 4, do you have any way of\
-        knowing which one best represents the distribution of all BPMs in your\
-        genre? Why or why not?',
+    'What does the x-axis of the histogram represent? What does the y-axis '+
+        'represent?',
+    'Select any two Beats Per Minute (BPM) values on the x-axis. How many '+
+        'data points have BPM values within that range?',
+    'The mean is the average value of a distribution of data points, and the '+
+        'standard deviation is a measure of how dispersed the data is in '+
+        'relation to the mean. Estimate the mean and standard deviation of '+
+        'your sample.',
+    'A new histogram is produced after every 100 data points. Draw additional '+
+        'data points to get you to 100 and take a screenshot of your '+
+        'histogram. Then draw another 100 data points, and paste a screenshot '+
+        'of the new histogram below. Is it identical to the first one? Why or '+
+        'why not?',
+    'Comparing the two histograms from question 4, do you have any way of '+
+        'knowing which one best represents the distribution of all BPMs in '+
+        'your genre? Why or why not?',
     'How could you improve your estimate of the average tempo for your genre?'
 ];
 
 const instructions: InstructionData[] = [
     {
-        instruction: 'Start by drawing 80 data points on the graph to start\
-            answering the following questions.'
+        instruction: 'Start by drawing 80 data points on the graph to start '+
+            'answering the following questions.'
     },
 ];
 
