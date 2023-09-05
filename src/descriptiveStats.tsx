960886a30dbd158e40b6aadd3ffb868d90ab8a8c
Evan Petersen <emp2221@columbia.edu>
2023-09-05 11:03:05 -0400

    Instructor Questions for Descriptive Statistics

--- src/descriptiveStats.tsx
+++ src/descriptiveStats.tsx
@@ -5,32 +5,36 @@ import { GraphForm } from './graphForm';
 import { Assignment } from './assignments/assignment';
 
 const descriptiveTabs: TabData[] = [
-    {title: 'Part 1.1', info: 'Etiam sagittis ornare libero, vitae blandit.'}
+    {
+        title: 'Part 1.1',
+        info: 'Etiam sagittis ornare libero, vitae blandit.'
+    }
 ];
 
 const questions: string[] = [
-    'Etiam sagittis ornare libero, vitae blandit orci sodales nec. Quisque tem'
-    + 'pus sem at ligula luctus lacinia. Aenean nec elementum nisl, sit amet v'
-    + 'olutpat mi. Sed quis purus nec sapien aliquam tristique. Sed',
-    'vitae lorem quis elit scelerisque sodales. Nulla ante libero, t'
-    + 'empus sit amet eros nec, convallis suscipit augue. Nam tempus'
-    + 'rutrum cursus. Ut lacinia blandit orci, eu viverra nulla. Sed '
-    + 'pulvinar urna orci, id dictum elit hendrerit a.'
+    'What does the x-axis of the histogram represent? What does the y-axis\
+        represent?',
+    'Select any two Beats Per Minute (BPM) values on the x-axis. How many data\
+        points have BPM values within that range?',
+    'The mean is the average value of a distribution of data points, and the\
+        standard deviation is a measure of how dispersed the data is in\
+        relation to the mean. Estimate the mean and standard deviation of your\
+        sample.',
+    'A new histogram is produced after every 100 data points. Draw additional\
+        data points to get you to 100 and take a screenshot of your histogram.\
+        Then draw another 100 data points, and paste a screenshot of the new\
+        histogram below. Is it identical to the first one? Why or why not?',
+    'Comparing the two histograms from question 4, do you have any way of\
+        knowing which one best represents the distribution of all BPMs in your\
+        genre? Why or why not?',
+    'How could you improve your estimate of the average tempo for your genre?'
 ];
 
 const instructions: InstructionData[] = [
-    {instruction:
-    'Etiam sagittis ornare libero, vitae blandit orci sodales nec. Quisque tem'
-    + 'pus sem at ligula luctus lacinia. Aenean nec elementum nisl, sit amet v'
+    {
+        instruction: 'Start by drawing 80 data points on the graph to start\
+            answering the following questions.'
     },
-    {instruction:
-    'olutpat mi. Sed quis purus nec sapien aliquam tristique. Sed' +
-    'vitae lorem quis elit scelerisque sodales. Nulla ante libero, t'
-    },
-    {instruction:
-    'empus sit amet eros nec, convallis suscipit augue. Nam tempus'
-    + 'rutrum cursus. Ut lacinia blandit orci, eu viverra nulla. Sed '
-    }
 ];
 
 export const DescriptiveStats: React.FC = () => {
